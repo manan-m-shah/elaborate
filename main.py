6782bf8f550e96274c1d421d80b82d0752bf5b0e
manan-m-shah <shahmananvyara@gmail.com>
2023-08-19 21:19:39 +0530

    🚀 AWS Lambda and Deployment

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from mangum import Mangum
 from fastapi import FastAPI
 from app.routes import github_routes
 
-# Create FastAPI instance
+# Creating FastAPI instance
 app = FastAPI()
 app.include_router(github_routes.router)
+
+# Wrapping the FastAPI app with Mangum for Lambda compatibility
+handler = Mangum(app)
